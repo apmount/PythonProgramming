5fd8b06b42cfd7bcf79961bd2afdb18a6cd837c6
apmount <91459566+apmount@users.noreply.github.com>
2021-10-01 13:26:13 +0300

    Add files via upload

---
+++ random_num.py
@@ -0,0 +1,58 @@
+import random
+
+i = 1
+points = 0
+RunAgain = True
+
+while RunAgain:
+
+    rnum = random.randint(1, 100)
+#    print(rnum)
+    for i in range(1, 11):
+
+        try:
+            guess = int(input("Guess a number: "))
+
+            if i != 10:
+                pass
+            else:
+                print("*** Last chance ***")
+
+            if guess == rnum:
+                print("Lucky you!!! You found it after ", i - 1, " failed tries")
+                points = 10 - i + 1
+                print('You have won ', points,' points')
+                break
+
+            elif guess < rnum:
+                if i == 10:
+                    print("Sorry mate, you lost")
+                    break
+                else:
+                    print("Aim higher...")
+
+            elif guess > rnum:
+                if i == 10:
+                    print("Sorry mate, you lost")
+                    break
+                else:
+                    print("Aim lower...")
+
+
+        except:
+           print("Please enter a number between 1 and 100")
+
+    select: str = input("Wanna try again? (Y/N): " )
+    if select == 'Y' or select == 'y':
+        continue
+
+    elif select == "N":
+        print("Bye bye!!!!")
+        break
+
+    else:
+        print("Goodbye!!")
+        break
+
+
+
