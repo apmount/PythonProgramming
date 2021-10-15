9c1a4c58601b90f8b2d9ef7d4b1c0ed263703d45
apmount <91459566+apmount@users.noreply.github.com>
2021-10-15 19:10:59 +0300

    Update random_num.py

--- random_num.py
+++ random_num.py
@@ -1,61 +1,75 @@
+# Random number game. Asks from user to guess a number between 1 and 100
+# that was generated from random function.
+#
+
 import random
 
-i = 1
 points = 0
 RunAgain = True
+score = []
 
 while RunAgain:
 
     rnum = random.randint(1, 100)
-#    print(rnum)
+    print(rnum)
     for i in range(1, 11):
 
         try:
-            guess = int(input("Guess a number: "))
-
-            if i != 10:
-                pass
-            else:
-                print("*** Last chance ***")
-
-            if guess == rnum:
-                print("Lucky you!!! You found it after ", i - 1, " failed tries")
-                points = 10 - i + 1
-                print('You have won ', points,' points')
+            guess = input("Guess a number (or exit to leave game): ")
+            if guess == 'exit':
+                print("Thank you for playing, hope to see you again!")
                 break
+            else:
+                guess = int(guess)
 
-            elif guess < rnum:
-                if i == 10:
-                    print("Sorry mate, you lost")
-                    break
+                if i != 10:
+                    pass
                 else:
-                    print("Aim higher...")
+                    print("*** Last chance ***")
 
-            elif guess > rnum:
-                if i == 10:
-                    print("Sorry mate, you lost")
+                if guess == rnum:
+                    print("Lucky you!!! You found it after ", i - 1, " failed tries")
+                    points = 10 - i + 1
+                    score.append(points)
+                    print("You have won ", points, " points")
                     break
-                else:
-                    print("Aim lower...")
+
+                elif guess < rnum:
+                    if i == 10:
+                        print("Sorry mate, you lost")
+                        break
+                    else:
+                        print("Aim higher...")
+
+                elif guess > rnum:
+                    if i == 10:
+                        print("Sorry mate, you lost")
+                        break
+                    else:
+                        print("Aim lower...")
 
         except:
-            if i!= 10:
-               print("Please enter a number between 1 and 100")
+            if i != 10:
+                print("Please enter a number between 1 and 100")
             else:
                 print("You lost!")
                 break
-                
-    select: str = input("Wanna try again? (Y/N): " )
-    if select == 'Y' or select == 'y':
-        continue
 
-    elif select == "N":
-        print("Bye bye!!!!")
-        break
-
+    if guess == "exit":
+        if len(score) == 0:
+            break
+        else:
+            print("Your score during each game was: ", score)
+            break
     else:
-        print("Goodbye!!")
-        break
-
-
-
+        select: str = input("Wanna try again? (Y/N): ")
+        if select == 'Y' or select == 'y':
+            continue
+        elif select == "N":
+            print("Bye bye!!!!")
+            print("Your score during each game was: ", score)
+            break
+        else:
+            print("Goodbye!!")
+            print("Your score during each game was: ", score)
+            break
